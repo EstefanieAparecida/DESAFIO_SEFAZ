5ec7372c76120f2d2fda6432fd0a0d26718fe5a5
Estefanie <Estefanieaparecida0@gmail.com>
2021-07-28 11:00:36 -0300

    inicialização projeto

---
+++ src/main/java/br/com/start/Main.java
@@ -0,0 +1,18 @@
+package br.com.start;
+
+import br.com.start.h2memory.H2MemoryDatabase;
+
+public class Main {
+	
+	public static void main (String[] args) {
+		
+		br.com.start.h2memory.H2MemoryDatabase H2MemoryDatabase = new H2MemoryDatabase();
+		H2MemoryDatabase.connect();
+			
+	}
+
+	
+
+	}
+
+
